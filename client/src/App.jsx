88cb17a226c3ed3df2adb742e43313afb74bcd5e
alwin antony <alwinanto5885@gmail.com>
2023-08-03 13:54:30 +0530

    FE : firebase config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import SignUp from './components/signup/Signup';
+import Login from './components/login/Login';
 
 function App() {
 
@@ -8,8 +9,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/signup' element={ <SignUp />} />
-          <Route path='/login' element={<>hi</>} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/config/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "pdfextractor-ea23f.firebaseapp.com",
+  projectId: "pdfextractor-ea23f",
+  storageBucket: "pdfextractor-ea23f.appspot.com",
+  messagingSenderId: "931616991508",
+  appId: "1:931616991508:web:d485726656311e4704ba01",
+  measurementId: "G-TB6CPVPDS6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+// const analytics = getAnalytics(app);
+
+export {app,auth,provider}
