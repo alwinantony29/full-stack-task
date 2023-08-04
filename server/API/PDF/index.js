decfd0550877e788f74ea15dbb892c8006dcc57c
alwin antony <alwinanto5885@gmail.com>
2023-08-04 20:00:09 +0530

    BE : updated multer configuration & pdf API to save metadata to DB

--- server/API/PDF/index.js
+++ server/API/PDF/index.js
@@ -1,26 +1,46 @@
 import express from "express";
-const Router=express.Router()
+const Router = express.Router()
 import multer from 'multer'
-
+import path from 'path'
+import PdfModel from "../../database/pdf/index.js";
+import { verifyToken } from "../../middleware/index.js";
 // Configuring Multer to handle file uploads
-const upload = multer({ dest: 'uploads/' }); // Destination folder for uploaded files
+const storage = multer.diskStorage({
+    destination: 'uploads/',
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        const extension = path.extname(file.originalname);
+        const originalFilenameWithoutExtension = path.parse(file.originalname).name;
+        const customFilename = originalFilenameWithoutExtension + '-' + uniqueSuffix + extension;
+        cb(null, customFilename);
+    },
+})
+const upload = multer({ storage });
 
-Router.get("/:pdfID",(req,res)=>{
+Router.get("/:pdfID", (req, res) => {
 
     console.log(req.params.pdfID);
     console.log("get api")
     res.send("hai bro")
-    
-})
-// 
-Router.post("/",upload.single('pdfFile'),(req,res)=>{
-    if (req.file) {
-        console.log("file nd");
-        console.log(req.file);
-    }
-    res.send("hai bro")
-    console.log("post api")
 
+})
+
+Router.post("/", verifyToken, upload.single('pdfFile'), async (req, res) => {
+    if (!req.file) {
+        console.log("No PDF file uploaded.");
+        return res.status(400).json({ message: 'No PDF file uploaded.' });
+    }
+    console.log("file nd");
+    console.log(req.file);
+    const { originalname, filename } = req.file
+    try {
+        const file = new PdfModel({ originalName: originalname, fileName: filename, userID: req.user._id })
+        await file.save()
+        res.json({ message: 'PDF file uploaded successfully.' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal server error : ' + err.message })
+    }
 })
 
 export default Router
